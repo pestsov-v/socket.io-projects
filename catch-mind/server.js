33e547de607affa2d6ce88301de5fc6eb9b0bbae
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-11 23:17:40 +0300

    connect to home pug

--- catch-mind/server.js
+++ catch-mind/server.js
@@ -1,1 +1,16 @@
+import {join} from 'path'
 import express from 'express'
+
+const PORT = 4001
+const app = express()
+app.set('view engine', 'pug')
+app.set('views', join(__dirname, "src/views"))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+
+const handleListening = () => console.log(`Server is running on http://localhost:${PORT}`)
+app.listen(PORT, handleListening)
