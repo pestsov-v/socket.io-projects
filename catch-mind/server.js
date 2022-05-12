b01e4c50f388c73297b824b80c307cd47f56df16
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-12 20:07:07 +0300

    socket chat

--- catch-mind/server.js
+++ catch-mind/server.js
@@ -1,16 +1,26 @@
-import {join} from 'path'
-import express from 'express'
+import {join} from 'path';
+import express from 'express';
+import socketIo from 'socket.io';
+import morgan from 'morgan'
 
-const PORT = 4001
-const app = express()
-app.set('view engine', 'pug')
-app.set('views', join(__dirname, "src/views"))
+const PORT = 4001;
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', join(__dirname, "src/views"));
+app.use(express.static(join(__dirname, 'src/static')));
+app.use(morgan('dev'));
+app.get('/', (req, res) => {res.render('home')});
 
-app.get('/', (req, res) => {
-    res.render('home')
+const handleListening = () => console.log(`Server is running on http://localhost:${PORT}`);
+const server = app.listen(PORT, handleListening);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    socket.on('newMessage', ({message}) => {
+        socket.broadcast.emit("messageNotification", { message, nickname: socket.nickname || 'Аноним' } )
+    })
+
+    socket.on('setNickname', ({nickname}) => {
+        socket.nickname = nickname;
+    })
 })
-
-
-
-const handleListening = () => console.log(`Server is running on http://localhost:${PORT}`)
-app.listen(PORT, handleListening)
---
+++ catch-mind/src/static/index.js
@@ -0,0 +1,20 @@
+const socket = io('/')
+socket.on('hello', () => {
+    console.log('Кто-то сказал привет')
+})
+
+function sendMessage(message) {
+    socket.emit("newMessage", { message })
+    console.log(`Вы: ${message}`)
+}
+
+function setNickname(nickname) {
+    socket.emit('setNickname', {nickname})
+}
+
+function handleMessageNotification(data) {
+    const {message, nickname } = data;
+    console.log(`${nickname}: ${message}`)
+}
+
+socket.on("messageNotification", handleMessageNotification)
